d5c3608b821e84343386bb9c5a2cc6d9f32c131c
Abdelrahman Ismael <a.ismael@bluskyint.com>
2024-09-30 11:14:12 +0300

    advanced 01

---
+++ Advanced_01/Advanced_01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Advanced_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Generic Range<T> class
+            //var intRange = new Range<int>(5, 15);
+            //Console.WriteLine(intRange.IsInRange(10));  // Output: True
+            //Console.WriteLine(intRange.IsInRange(20));  // Output: False
+            //Console.WriteLine("Length of range: " + intRange.Length());  // Output: 10
+            //Console.WriteLine(intRange);  // Output: Range: [5, 15]
+
+            //var doubleRange = new Range<double>(1.5, 3.8);
+            //Console.WriteLine(doubleRange.IsInRange(2.5));  // Output: True
+            //Console.WriteLine("Length of range: " + doubleRange.Length());  // Output: 2.3
+            #endregion
+
+            #region ArrayList containing a sequence of elements
+            ArrayList arrayList = new ArrayList() { 1, 2, 3, 4, 5 };
+            foreach (var item in arrayList)
+            {
+                Console.Write(item + " ");
+            }
+            ReversArrayElements(arrayList);
+            foreach (var item in arrayList)
+            {
+                Console.Write(item + " ");
+            }
+            #endregion
+        }
+        public static void ReversArrayElements(ArrayList list)
+        {
+            object[] tempArray = new object[list.Count];
+
+            // Copy elements in reverse order into tempArray
+            for (int i = 0; i < list.Count; i++)
+            {
+                tempArray[i] = list[list.Count - 1 - i];
+            }
+
+            // Copy elements back to the original ArrayList
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] = tempArray[i];
+            }
+        }
+    }
+}
---
+++ Advanced_01/Advanced_01/Range.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_01
+{
+    internal class Range<T> where T : IComparable<T> // T Must impelement this IComparable interface 
+    {
+        public T Min { get; private set; }
+        public T Max { get; private set; }
+
+        public Range(T min, T max)
+        {
+            if(min.CompareTo(max) < 0)
+            {
+                throw new ArgumentException("Minimum value cannot be greater than Maximum value.");
+            }
+            this.Min = min;
+            this.Max= max;
+        }
+
+        // Method to check if a value is within the range
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+
+        // Method to get the length of the range (difference between max and min)
+        public T Length()
+        {
+            dynamic max = Max;
+            dynamic min = Min;
+            return  max - min;
+        }
+
+        // Override ToString method
+        public override string ToString()
+        {
+            return $"Range: [{Min}, {Max}]";
+        }
+    }
+}
